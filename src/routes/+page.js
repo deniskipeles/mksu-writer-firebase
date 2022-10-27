b476f0c0b1f4fcdd165077d4ed4b2fb096dbde82
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2022-10-27 13:43:18 +0300

    Update +page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,3 +1,32 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+import { collection, query, orderBy, startAfter, limit, getDocs } from "firebase/firestore";  
+import {app,db} from '$lib/firebase.js';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+ 
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+    const articles = await getArticles()
+    // const lastVisible = await (await pageOne()).lastVisible
+    console.log(articles)
+    return {
+      articles,
+      // lastVisible
+    };
+}
+
+
+
+async function getArticles() {
+    const articlesCol = collection(db, 'article');
+    const articleSnapshot = await (await getDocs(articlesCol));
+    const articleList = articleSnapshot.docs.map(doc => {
+            const data=doc.data()
+            return{
+              id:doc.id,
+              title:data.title,
+              photos:data.photos,
+              text:data.text,
+            }
+    });
+    return articleList;
+}
